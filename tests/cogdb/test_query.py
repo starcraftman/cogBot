d62d3457468fe94e53e820177e41f4ae316a6ba2
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2017-06-04 20:08:37 -0400

    Add stub for fuzzy_find and test data.

--- tests/cogdb/test_query.py
+++ tests/cogdb/test_query.py
@@ -10,6 +10,50 @@ import cog.exc
 import cogdb.query
 
 
+SYSTEMS = ["Frey", "Nurundere", "LHS 3749", "Sol", "Dongkum", "Alpha Fornacis",
+           "Phra Mool", "LP 291-34", "Wat Yu", "Rana", "Adeo", "Mariyacoch",
+           "LTT 15449", "Gliese 868", "Shoujeman", "Anlave", "Atropos", "16 Cygni",
+           "Abi", "LHS 3447", "Lalande 39866", "Phanes", "NLTT 46621", "Othime",
+           "Aornum", "Wolf 906", "LP 580-33", "BD+42 3917", "37 Xi Bootis", "Mulachi",
+           "Wolf 25", "LHS 6427", "39 Serpentis", "Bhritzameno", "Gilgamesh",
+           "Epsilon Scorpii", "Ross 33", "Kaushpoos", "LHS 142", "Venetic", "LHS 1541",
+           "Parutis", "Wolf 867", "Vega", "Groombridge 1618", "Lushertha", "LHS 3885",
+           "G 250-34", "Tun", "Lung", "LHS 3577", "LTT 15574", "GD 219", "LHS 1197",
+           "WW Piscis Austrini", "LPM 229"
+          ]
+
+USERS = ["Alexander Astropath", "Toliman", "TiddyMun", "Oskiboy[XB1/PC]",
+         "Winna09", "Shepron", "Grimbald", "Haphollas", "Gary Brain", "Ricshah",
+         "Rico Char", "GearsandCogs", "NotRjwhite", "Rumrunner", "A Name With Spaces"
+        ]
+
+
+class CouldNotMatchOne(Exception):
+    pass
+
+
+def fuzzy_find(needle, stack):
+    """
+    Simple fuzzy find based on needle.
+
+    If matches more than one element in stack, raise CouldNotMatchOne
+    """
+    # FIXME: Very simple stub, change later to subsequence.
+    new_stack = []
+    for line in stack:
+        if needle in line:
+            new_stack.append(line)
+
+    if len(new_stack) != 1:
+        raise CouldNotMatchOne
+
+    return new_stack[0]
+
+
+def test_fuzzy_find():
+    assert fuzzy_find('Alex', USERS) == 'Alexander Astropath'
+
+
 def test_first_system_column():
     col_start = cogdb.query.first_system_column(json.loads(FMT_CELLS))
     assert col_start == 'F'
