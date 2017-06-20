ff93e79d180b5dfc2fbe432373a6d73e3bdcf40d
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2017-06-20 19:42:12 -0400

    Check version on cog start.
    - Simplify tox.ini

---
+++ cog/__init__.py
@@ -0,0 +1,14 @@
+"""
+Simple check, this whole module fails on python < 3.5.x
+See cog/bot.py
+"""
+from __future__ import absolute_import, print_function
+import sys
+
+
+try:
+    assert sys.version_info[0:2] >= (3, 5)
+except AssertionError:
+    print('This entire program must be run with python >= 3.5')
+    print('If unavailable on platform, see https://github.com/pyenv/pyenv')
+    sys.exit(1)
--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ except NameError:
 try:
     assert sys.version_info[0:2] >= (3, 5)
 except AssertionError:
-    print('This entire program must be run with python >= 3.5.')
+    print('This entire program must be run with python >= 3.5')
     print('If unavailable on platform, see https://github.com/pyenv/pyenv')
     sys.exit(1)
 
