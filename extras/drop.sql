d1acb84dab4c83cee734a231abf9d3881c86c7d7
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2017-09-05 11:33:58 -0400

    Small script, clears db on execute.

---
+++ extras/drop.sql
@@ -0,0 +1,6 @@
+drop table if exists um_merits;
+drop table if exists merits;
+drop table if exists um_systems;
+drop table if exists systems;
+drop table if exists sheet_users;
+drop table if exists discord_users;
