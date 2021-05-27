db77f14888d7c5eae5f58d25498378a4cf667c65
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2021-05-27 09:41:49 -0400

    Bump pool sizes a bit.

--- cogdb/__init__.py
+++ cogdb/__init__.py
@@ -37,7 +37,7 @@ else:
     CUR_DB = os.environ.get('COG_TOKEN', 'dev')
     CREDS['db'] = CUR_DB
 
-engine = sqlalchemy.create_engine(MYSQL_SPEC.format(**CREDS), echo=False, pool_recycle=3600, pool_size=10)
+engine = sqlalchemy.create_engine(MYSQL_SPEC.format(**CREDS), echo=False, pool_recycle=3600, pool_size=20)
 Session = sqlalchemy.orm.sessionmaker(bind=engine)
 logging.getLogger(__name__).error('Main Engine Selected: %s', engine)
 
@@ -48,7 +48,7 @@ EDDBSession = sqlalchemy.orm.sessionmaker(bind=eddb_engine)
 
 # Remote server tracking bgs
 CREDS = cog.util.get_config('dbs', 'side')
-side_engine = sqlalchemy.create_engine(MYSQL_SPEC.format(**CREDS), echo=False, pool_recycle=3600, pool_size=5)
+side_engine = sqlalchemy.create_engine(MYSQL_SPEC.format(**CREDS), echo=False, pool_recycle=3600, pool_size=10)
 SideSession = sqlalchemy.orm.sessionmaker(bind=side_engine)
 
 CREDS = None
