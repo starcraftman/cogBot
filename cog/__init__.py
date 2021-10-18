d01a719da3d3a8a756ce177227f1966c4d48b1ca
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2021-10-18 06:20:42 -0400

    Revert "Lower back to 3.6.X"
    This reverts commit 24c98c06b68c2361564b24af83afdbc36757f044.
    - Don't make changes late at night.

--- cog/__init__.py
+++ cog/__init__.py
@@ -6,7 +6,7 @@ import sys
 __version__ = '0.3.1'
 
 try:
-    assert sys.version_info[0:2] >= (3, 6)
+    assert sys.version_info[0:2] >= (3, 7)
 except AssertionError:
     print('This entire program must be run with python >= 3.7')
     print('If unavailable on platform, see https://github.com/pyenv/pyenv')
