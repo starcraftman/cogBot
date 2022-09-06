277ba08acf47e08e01d542408176fcf7fb413b27
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2022-09-06 16:37:30 -0400

    Fully implement a webscraper for the powerplay spy page.
    - Some testing omitted where it would require the webdriver.
    - Proof of concept mainly, needs to be hooked into database.

--- cogdb/scrape.py
+++ cogdb/scrape.py
@@ -1,51 +1,271 @@
 """
 Scrape the json site. This is a stopgap.
+
+    0) Remove all "chromium" packages, driver needs chrome
+    1) Install google chrome
+        wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    2) Fetch corresponding chromedriver
+        https://chromedriver.chromium.org/downloads
+    3) Configure 'scrape.url' and 'scrape.driver' in the config.
 """
+import contextlib
+import datetime
+import json
+import logging
 import os
+import re
+import time
 
 import bs4
-#  import scrapy
-from selenium import webdriver
+import selenium
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
 import cog.util
+import cogdb
+import cogdb.eddb
+from cogdb.eddb import Power
 
-DRIVER_PATH = "/home/starcraftman/Downloads/chromedriver"
-# Must be actual google chrome project
-#   wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+# All gaps in seconds
+SHORT_GAP = 0.1
+LONG_GAP = 2
+LONGEST_GAP = 15
+# Regex below
+# Match a line for held merits where it isn't unknown
+MAT_MERITS = re.compile(r".*Fortification: (\d+) / (\d+).*Undermining: (\d+) / (\d+).*Held Merits: (\d+) stolen and (\d+) held \(\+ (\d+) (.*)")
+# Match a line for held merits where it is unknown
+MAT_UKNOWN = re.compile(r".*Fortification: (\d+) / (\d+).*Undermining: (\d+) / (\d+).*Held Merits: unknown")
+# Matchers to parse time out of the held merits line
+MAT_TIME = {
+    'seconds': re.compile(r'(\d+) seconds?'),
+    'minutes': re.compile(r'(\d+) minutes?'),
+    'hours': re.compile(r'(\d+) hours?'),
+    'days': re.compile(r'(\d+) days?'),
+}
+HELD_MERITS_RECENT = 60 * 60  # Within last hour
 
 
-def powerplay_for(driver, name):
+@contextlib.contextmanager
+def get_chrome_driver(dev=True):  # pragma: no cover | Just a context wrapper around library startup
+    """Initialize the chrome webdriver.
+
+    This is a context object, use with with.
+
+    Args:
+        dev: When False, will run in headless mode. Otherwise GUI is run.
+    """
+    driver = None
+    try:
+        options = Options()
+        options.add_argument("--window-size=1920x1080")
+        if not dev:
+            options.headless = True
+
+        service = Service(ChromeDriverManager().install())
+        driver = selenium.webdriver.Chrome(service=service, options=options)
+        yield driver
+    finally:
+        if driver:
+            driver.quit()
+
+
+def powerplay_names():
+    """The names of all powerplay leaders.
+
+    Returns: A list of all powerplay leaders sorted alphabetically.
+    """
+    with cogdb.session_scope(cogdb.EDDBSession) as eddb_session:
+        powers = eddb_session.query(Power.text).\
+            filter(Power.text != "None").\
+            order_by(Power.text).\
+            all()
+        return [x[0] for x in powers]
+
+
+def parse_date(text, *, start):
+    """Parse date from powerplay page source block.
+
+    The date is relatively in past (i.e. of format x days, y minutes ago).
+
+    Args:
+        text: The text block to extract date info from.
+        start: The starting datetime object (should be utc).
+
+    Returns: A datetime offset from start that is time this info was updated, 0 if no info present.
+    """
+    if ": unknown" in text or "as of" not in text:
+        return 0
+
+    text = text[text.rindex("as of"):]
+    kwargs = {}
+
+    for name, mat in MAT_TIME.items():
+        found = mat.findall(text)
+        kwargs[name] = int(found[0]) if found else 0
+
+    return start - datetime.timedelta(**kwargs)
+
+
+def powerplay_leader(driver, leader_index, held_merits=False):
+    """Scrape the powerplay page for the spy site.
+
+    Expectation: Driver is already loaded and pointed at powerplay page.
+
+    Args:
+        driver: A webdriver, should be chrome.
+        leader_index: The index of the leader you want to parse, 1 index
+        held_merits: If True, fetch held merits for all systems on page.
+                     Estimate, 30s a system. Will block and return once all fetched.
+    """
     # Fetch the page for named leader
-    top_box = driver.find_element_by_class_name("blazored-typeahead__input")
-    # Clear input first?
-    top_box.send_keys(name)
-    top_box.send_keys(Keys.RETURN)
+    top_box = driver.find_element(By.CLASS_NAME, "blazored-typeahead__input-icon")
+    top_box.click()
+    time.sleep(LONG_GAP)
 
-    # select all boxes and then fetch
-    for box in driver.find_elements_by_class_name("checkmark"):
-        box.click()
+    # Select leader from drop down by index
+    selects = driver.find_elements(By.CLASS_NAME, "blazored-typeahead__result ")
+    selects[leader_index].click()
+    now = datetime.datetime.utcnow()  # Time is relative when you click leader in list
+    time.sleep(LONG_GAP)
 
-    print(bs4.BeautifulSoup(driver.page_source, 'html.parser').prettify())
+    if held_merits:
+        for check in driver.find_elements(By.CLASS_NAME, "checkmark"):
+            check.click()
+            time.sleep(SHORT_GAP)
+
+        # Get held merits fro all selected systems of power
+        buttons = driver.find_elements(By.CSS_SELECTOR, "button.btn-primary")
+        buttons[0].click()
+
+        print("Wait start:", str(now))
+        while not check_held_recent(driver.page_source, now):
+            time.sleep(LONGEST_GAP)
+        end = datetime.datetime.utcnow()
+        print("Wait end:", str(now))
+        print("Wait diff:", str(end - now))
+
+    # Parse all information on the page
+    info = parse_powerplay_page(driver.page_source, start=now)
+
+    # Clear the selection for next time.
+    clear = driver.find_element(By.CLASS_NAME, "blazored-typeahead__clear")
+    clear.click()
+    time.sleep(LONG_GAP)
+
+    return info
+
+
+def scrape_all_powerplay(driver, held_merits=False):  # pragma: no cover | Depends on driver running.
+    """Scrape all powerplay factions.
+
+    Args:
+        driver: The webdriver, probably chrome.
+        held_merits: If True, fetch held merits for all powerplay leaders.
+                     WARNING: VERY SLOW
+    """
+    url = os.path.join(cog.util.CONF.scrape.url, 'powerplay')
+    driver.get(url)
+    time.sleep(LONG_GAP)
+
+    # Expand all blocks for information
+    switch = driver.find_element(By.CLASS_NAME, "input-switch")
+    switch.click()
+
+    return {
+        leader: powerplay_leader(driver, ind, held_merits=held_merits)
+        for ind, leader in enumerate(powerplay_names())
+    }
+
+
+def check_held_recent(page_source, *, start):
+    """Check if any of the outstanding requests are still unknown or too old.
+
+    A recent held merits is present and less than 1 hour old.
+
+    Args:
+        page_source: The page source, a string.
+        start: The start timestamp of page load.
+
+    Returns: Boolean True iff all parts of page are showing recent held.
+    """
+    soup = bs4.BeautifulSoup(page_source, 'html.parser')
+
+    for body in soup.find_all("div", {"class": "accordion-body"}):
+        text = str(body)
+        if MAT_UKNOWN.match(text):
+            return False
+
+        updated_at = parse_date(text, start=start)
+        if (start - updated_at).total_seconds() > HELD_MERITS_RECENT:
+            return False
+
+    return True
+
+
+def parse_powerplay_page(page_source, *, start):
+    """Parse the systems and information from the current powerplay page.
+
+    Args:
+        page_source: The page source, a string.
+        start: The start timestamp of page load.
+
+    Return: A dictionary of form: system_name -> info dict.
+    """
+    soup = bs4.BeautifulSoup(page_source, 'html.parser')
+
+    # Find all names of the systems
+    headers = soup.find_all("h2", {"class": "accordion-header"})
+    systems = [{"name": header.text.strip()} for header in headers]
+
+    # Extract info by regex from all expanded info blocks
+    bodies = soup.find_all("div", {"class": "accordion-body"})
+    for ind, body in enumerate(bodies):
+        info = {
+            'held': 0,
+            'redeemed': 0,
+            'held_date': 0,
+        }
+
+        # If the held merits available parse them out
+        mat = MAT_MERITS.match(str(body))
+        if mat:
+            info = {
+                'held': int(mat.group(5)) + int(mat.group(6)),
+                'redeemed': int(mat.group(7)),
+                'held_date': int(parse_date(str(body), start=start).timestamp()),
+            }
+        else:  # Held merits were not available.
+            logging.getLogger(__name__).error("Held Merits missing: %s", systems[ind]['name'])
+            mat = MAT_UKNOWN.match(str(body))
+
+        info.update({
+            "fort": int(mat.group(1)),
+            "fort_trigger": int(mat.group(2)),
+            "um": int(mat.group(3)),
+            "um_trigger": int(mat.group(4)),
+        })
+        systems[ind].update(info)
+
+    return {x['name']: x for x in systems}
 
 
 def main():
-    try:
-        url = os.path.join(cog.util.CONF.scrape, 'powerplay')
-        options = Options()
-        #  options.headless = True
-        #  options.add_argument("--windo-size=960x960")
-        driver = webdriver.Chrome(executable_path=DRIVER_PATH, options=options)
-        driver.get(url)
+    """
+    Demonstrate a complete parsing of all powerplay information and dump to json file.
+    """
+    out_file = "/tmp/data.json"
+    # Run a sanity test, parse entire powerplay page for all leaders.
+    with get_chrome_driver(dev=True) as driver:
+        data = scrape_all_powerplay(driver, held_merits=False)
+        with open(out_file, "w") as fout:
+            fout.write(json.dumps(data, sort_keys=True, indent=2))
 
-        leader = "Zachary Hudson"
-        powerplay_for(driver, leader)
-
-        # Fetch the page for hudson
-        driver.get(url)
-    finally:
-        driver.quit()
+    print("Parsing all powerplay data is COMPLETE!")
+    print(f"JSON info can be found in: {out_file}")
 
 
 if __name__ == "__main__":
---
+++ tests/cogdb/test_scrape.py
@@ -0,0 +1,100 @@
+"""
+Tests for cogdb.scrape
+"""
+import datetime
+import os
+
+import pytest
+
+import cog.util
+import cogdb.scrape
+
+
+@pytest.fixture
+def now():
+    yield datetime.datetime.utcfromtimestamp(1662490449)
+
+
+def test_powerplay_names():
+    names = cogdb.scrape.powerplay_names()
+    assert len(names) == 11
+    assert "Zachary Hudson" in names
+
+
+def test_parse_date_unknown(now):
+    expect = 0
+    text = "Held Merits: unknown"
+
+    assert expect == cogdb.scrape.parse_date(text, start=now)
+
+
+def test_parse_date_all(now):
+    expect = "2022-09-05 15:23:31"
+    text = "Held Merits: 0 stolen and 0 held (+ 0 = 0 / 11598) as of 1 days, 3 hours, 30 minutes, 38 seconds ago."
+
+    assert expect == str(cogdb.scrape.parse_date(text, start=now))
+
+
+def test_parse_date_partial(now):
+    expect = "2022-09-05 18:24:09"
+    text = "Held Merits: 0 stolen and 0 held (+ 0 = 0 / 11598) as of 1 days, 30 minutes ago."
+
+    assert expect == str(cogdb.scrape.parse_date(text, start=now))
+
+
+def test_check_held_recent_unknown(now):
+    assert not cogdb.scrape.check_held_recent(HELD_UNKNOWN, start=now)
+
+
+def test_check_held_recent_is_old(now):
+    assert not cogdb.scrape.check_held_recent(HELD_OLD, start=now)
+
+
+def test_check_held_recent_is_recent(now):
+    assert cogdb.scrape.check_held_recent(HELD_RECENT, start=now)
+
+
+def test_parse_powerplay_page(now):
+    expect_held = {
+        'fort': 4464,
+        'fort_trigger': 4247,
+        'held': 0,
+        'held_date': 1662504156,
+        'name': 'Aowica',
+        'redeemed': 0,
+        'um': 0,
+        'um_trigger': 11598
+    }
+    expect_unknown = {
+        'fort': 2976,
+        'fort_trigger': 2842,
+        'held': 0,
+        'held_date': 0,
+        'name': 'Zhao',
+        'redeemed': 0,
+        'um': 0,
+        'um_trigger': 20067
+    }
+
+    parsed = cogdb.scrape.parse_powerplay_page(WHOLE_PAGE, start=now)
+    assert expect_held == parsed['Aowica']
+    assert expect_unknown == parsed['Zhao']
+
+
+HELD_UNKNOWN = """
+<div class="powerplay-result-box"><!--!--><!--!--><div class="accordion-item"><h2 class="accordion-header"><button type="button" aria-expanded="true" data-bs-toggle="collapse" class="accordion-button " _bl_20634afa-abeb-4fd8-811c-ffb4e8e50b71=""><div style="display: table;"><input type="checkbox" class="checkmark"><!--!-->
+                    <p style="display: table-cell; vertical-align: middle; padding-left: 1em;">Aowica</p></div></button></h2><!--!-->
+<div class="accordion-collapse collapse show" data-blazorstrap="434678de-c20e-4f09-8230-7ef4ac4ce688" _bl_8f7632f8-0a9c-48f1-983f-afdcce3f8793="" style=""><div class="accordion-body"><!--!--><!--!-->Fortification: 4464 / 4247<br>Undermining: 0 / 11598<br><br>Held Merits: unknown.<!--!-->
+"""
+HELD_RECENT = """
+<div class="powerplay-result-box"><!--!--><!--!--><div class="accordion-item"><h2 class="accordion-header"><button type="button" aria-expanded="true" data-bs-toggle="collapse" class="accordion-button " _bl_20634afa-abeb-4fd8-811c-ffb4e8e50b71=""><div style="display: table;"><input type="checkbox" class="checkmark"><!--!-->
+                    <p style="display: table-cell; vertical-align: middle; padding-left: 1em;">Aowica</p></div></button></h2><!--!-->
+<div class="accordion-collapse collapse show" data-blazorstrap="434678de-c20e-4f09-8230-7ef4ac4ce688" _bl_8f7632f8-0a9c-48f1-983f-afdcce3f8793="" style=""><div class="accordion-body"><!--!--><!--!-->Fortification: 4464 / 4247<br>Undermining: 0 / 11598<br><br>Held Merits: 0 stolen and 0 held (+ 0 = 0 / 11598) as of 11 minutes, 33 seconds ago.<!--!-->
+"""
+HELD_OLD = """
+<div class="powerplay-result-box"><!--!--><!--!--><div class="accordion-item"><h2 class="accordion-header"><button type="button" aria-expanded="true" data-bs-toggle="collapse" class="accordion-button " _bl_20634afa-abeb-4fd8-811c-ffb4e8e50b71=""><div style="display: table;"><input type="checkbox" class="checkmark"><!--!-->
+                    <p style="display: table-cell; vertical-align: middle; padding-left: 1em;">Aowica</p></div></button></h2><!--!-->
+<div class="accordion-collapse collapse show" data-blazorstrap="434678de-c20e-4f09-8230-7ef4ac4ce688" _bl_8f7632f8-0a9c-48f1-983f-afdcce3f8793="" style=""><div class="accordion-body"><!--!--><!--!-->Fortification: 4464 / 4247<br>Undermining: 0 / 11598<br><br>Held Merits: 0 stolen and 0 held (+ 0 = 0 / 11598) as of 1 hours, 11 minutes, 33 seconds ago.<!--!-->
+"""
+with open(os.path.join(cog.util.ROOT_DIR, 'tests', 'cogdb', 'whole_page.html')) as fin:
+    WHOLE_PAGE = fin.read()
